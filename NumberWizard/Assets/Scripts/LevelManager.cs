81a02bac9cf227a58bfa0ff545d63b7f30fffa04
mahyar121 <mahyarhajibabaie@gmail.com>
2016-12-24 15:10:57 -0800

    Implemented a Back Button

--- NumberWizard/Assets/Scripts/LevelManager.cs
+++ NumberWizard/Assets/Scripts/LevelManager.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name)
     {
-        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene("Game");
     }
 
     public void QuitRequest()
     {
-        Debug.Log("I want to quit!");
+        Application.Quit();
+    }
+
+    public void BackRequest()
+    {
+        SceneManager.LoadScene("Start");
     }
 }
